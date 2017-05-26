5fe7db580fbdd3288a5510756afb0b1924e6f311
Natanael Ahac <natanael.ahac@gmail.com>
2017-05-27 01:54:11 +0200

    lot of stuff

--- models/recipe.js
+++ models/recipe.js
@@ -3,26 +3,27 @@ var SearchRequest = require('../models/searchRequest');
 
 class Recipe {
     constructor(recipeId, title, description, isPublic, userId, pictureId, preparationTime) {
-        if(recipeId !== undefined && recipeId > 0)
+        if (recipeId !== undefined && recipeId > 0)
             this.recipeId = recipeId;
         this.title = title;
         this.description = description;
         this.public = isPublic;
         this.userId = userId;
         this.preparationTime = preparationTime;
-        if(pictureId !== undefined)
+        if (pictureId !== undefined)
             this.pictureId = pictureId;
     }
 
     static getRecipesByUserId(userId, callback) {
-        new db.Recipes({userId : userId})
-        .fetchAll({withRelated: ['ingredients', 'preparationSteps']})
-        .then((model) => {
-            if(model === null)
-                callback({success: false});
-            else
-                callback({success: true, data : model});
-        });
+        new db.Recipes()
+            .where('userId', '=', userId)
+            .fetchAll({withRelated: ['ingredients', 'preparationSteps']})
+            .then((model) => {
+                if (model === null)
+                    callback({success: false});
+                else
+                    callback({success: true, data: model});
+            });
     }
 
     static searchRecipe(data, callback) {
@@ -33,10 +34,10 @@ class Recipe {
             .where('preparationTime', '<', searchReques.maxPreparationTime)
             .fetchAll({withRelated: ['ingredients', 'preparationSteps']})
             .then((model) => {
-                if(model === null)
+                if (model === null)
                     callback({success: false});
                 else
-                    callback({success: true, data : model});
+                    callback({success: true, data: model});
             })
             .catch((error) => {
                 console.log("error search");
@@ -45,34 +46,34 @@ class Recipe {
     }
 
     static getRecipeById(recipeId, callback) {
-        new db.Recipes({recipeId : recipeId})
+        new db.Recipes({recipeId: recipeId})
             .fetch()
             .then((model) => {
-                if(model === null)
+                if (model === null)
                     callback({success: false});
                 else
-                    callback({success: true, data : model});
+                    callback({success: true, data: model});
             });
     }
 
     static saveRecipe(recipeId, title, description, isPublic, userId, pictureId, preparationTime, callback) {
-            let recipe = new Recipe(recipeId, title, description, isPublic, userId, pictureId, preparationTime);
-            new db.Recipes(recipe)
-                .save(null, {  })
-                .then((recipe) => {
-                    console.log("recipe inserted or updated");
-                    callback(true, recipe);
-                })
-                .catch((error) => {
-                    console.log("recipe insert or update error");
-                    callback(false);
-                });
+        let recipe = new Recipe(recipeId, title, description, isPublic, userId, pictureId, preparationTime);
+        new db.Recipes(recipe)
+            .save(null, {})
+            .then((recipe) => {
+                console.log("recipe inserted or updated");
+                callback(true, recipe);
+            })
+            .catch((error) => {
+                console.log("recipe insert or update error");
+                callback(false);
+            });
     }
 
     static updateRecipe(recipeId, title, description, isPublic, userId, pictureId, preparationTime, callback) {
         let recipe = new Recipe(recipeId, title, description, isPublic, userId, pictureId, preparationTime);
         new db.Recipes(recipe)
-            .save(null, { method: 'insert' })
+            .save(null, {method: 'insert'})
             .then((recipe) => {
                 console.log("recipe inserted");
                 callback(true, recipe);
